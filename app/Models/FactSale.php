0eb364d532d1ad9cc2d7c707a3485c122de4b969
Nurrachman Budi Mulya <nurrachmanbudimulua@gmail.com>
2022-07-03 11:00:41 +0700

    New Model FactSales

--- app/Models/FactSale.php
+++ app/Models/FactSale.php
@@ -11,15 +11,45 @@ class FactSale extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'ship_id',
-        // 'date',
-        // 'amount',
+    protected  $fillable = [
+        'TimeID',
+        'CustomerID',
+        'StoreID',
+        'ProductionID',
+        'AddressID',
+        'EmployeeID',
+        'ShipMethodID',
+
     ];
 
-    // belongs to Ship
-    public function ship(): BelongsTo
+    // belongs to
+
+    public function time(): BelongsTo
     {
-        return $this->belongsTo(Ship::class, 'ship_id');
+        return $this->belongsTo(Time::class, 'TimeID');
+    }
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'CustomerID');
+    }
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class, 'StoreID');
+    }
+    public function production(): BelongsTo
+    {
+        return $this->belongsTo(Production::class, 'ProductionID');
+    }
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class, 'AddressID');
+    }
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'EmployeeID');
+    }
+    public function shipmethod(): BelongsTo
+    {
+        return $this->belongsTo(ShipMethod::class, 'ShipMethodID');
     }
 }
