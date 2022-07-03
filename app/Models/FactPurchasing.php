34e1e8df21526049c0c3180761879da86fc2d73a
Nurrachman Budi Mulya <nurrachmanbudimulua@gmail.com>
2022-07-03 11:16:53 +0700

    Update Model FactPurchasing

--- app/Models/FactPurchasing.php
+++ app/Models/FactPurchasing.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class FactPurchasing extends Model
 {
     use HasFactory;
+
+    protected  $guarded = [
+        'id',
+    ];
+
+    // belongs to
+
+    public function time(): BelongsTo
+    {
+        return $this->belongsTo(Time::class, 'TimeID');
+    }
+    public function vendor(): BelongsTo
+    {
+        return $this->belongsTo(Vendor::class, 'VendorID');
+    }
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'ProductID');
+    }
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'EmployeeID');
+    }
+    public function shipmethod(): BelongsTo
+    {
+        return $this->belongsTo(ShipMethod::class, 'ShipMethodID');
+    }
 }
